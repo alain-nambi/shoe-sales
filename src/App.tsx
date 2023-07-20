943973a55b26d130a1bfc0c94ec9987376d12c7a
alain-nambi <alainnambi@gmail.com>
2023-07-20 11:32:29 +0300

    update: import Footer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/globals.css'
 import NavBar from './components/navbar/navbar'
 import Container from './components/container'
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Container>
         <NavBar />
       </Container>
+      <Footer />
     </>
   )
 }
