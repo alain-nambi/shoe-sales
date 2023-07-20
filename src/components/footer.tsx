93faff3b1713e31287f442904711177a6b59b4e6
alain-nambi <alainnambi@gmail.com>
2023-07-20 11:32:16 +0300

    feat(ui): add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+        <div className="text-center border border-b-0 py-6 mt-6">
+            <span className="text-sm">Copyright &copy; {new Date().getFullYear()}. All right reserved </span>
+        </div>
+    )
+}
+
+export default Footer
