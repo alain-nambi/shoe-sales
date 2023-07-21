ed43a9598002edfd1e6f52e6bf95660eaca73949
alain-nambi <alainnambi@gmail.com>
2023-07-21 15:26:48 +0300

    feat(ui): update NavBar component * Add useMediaQuery * Import icons from lucide-react * Import styles from navbar modules css

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,54 +1,137 @@
-import { Button } from "../ui/button"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import {
+  Home,
+  Info,
+  Phone,
+  ShoppingBag,
+  GalleryVerticalEnd,
+  Minimize2,
+  LogIn,
+  UserPlus2,
+} from "lucide-react";
+
+import styles from "./navbar.module.css"
 
 const NavBar: React.FC = () => {
-    return (
-        <>
-            {/* Navigation bar */}
-            <nav className="flex justify-between items-center py-3">
-                {/* Logo */}
-                <div className="text-base text-orange-500 font-black">
-                    <span className="cursor-pointer">Logo</span>
-                </div>
-                {/* Navigation link */}
-                <ul className="flex gap-5 text-sm cursor-pointer">
-                    <NavItem href="/">Home</NavItem>
-                    <NavItem href="/about">About</NavItem>
-                    <NavItem href="/category">Category</NavItem>
-                    <NavItem href="/contact">Contact</NavItem>
-                </ul>
-                {/* Navigation buttons */}
-                <div className="flex items-center gap-2">
-                    <Button className="bg-orange-500 hover:bg-orange-400">Login</Button>
-                    <Button
-                        className="
-                            border-orange-500 
-                            border bg-transparent 
-                            hover:border-orange-400 
-                            text-orange-500 
-                            hover:bg-transparent 
-                            hover:text-orange-400
-                        "
-                    >
-                        Signup
-                    </Button>
-                </div>
-            </nav>
-        </>
-    )
-}
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+
+  const navItemsDesktop = [
+    { href: "/", label: "Home", icon: Home },
+    { href: "/about", label: "About", icon: Info },
+    { href: "/category", label: "Category", icon: ShoppingBag },
+    { href: "/contact", label: "Contact", icon: Phone },
+  ];
+
+  const navItemsMobile = [
+    ...navItemsDesktop,
+    { href: "/login", label: "Login", icon: LogIn },
+    { href: "/sign-up", label: "Sign Up", icon: UserPlus2 },
+  ];
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      {/* Navigation bar */}
+      <nav className="flex justify-between items-center py-3">
+        {/* Logo */}
+        <div
+          className={`flex justify-between items-center ${
+            isMobile ? "w-full" : ""
+          }`}
+        >
+          <div className="text-base text-orange-500 font-black">
+            <span className="cursor-pointer">Logo</span>
+          </div>
+
+          {/* Navigation buttons for mobile devices */}
+          {/* Ajouter les boutons de navigation pour les mobiles */}
+          {isMobile && (
+            <Button
+              className={`${styles.buttonPrimary}`}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              title={`${
+                isMobileMenuOpen ? "Fermer le menu" : "Ouvrir le menu"
+              }`}
+            >
+              {isMobileMenuOpen ? (
+                <Minimize2 size={20} />
+              ) : (
+                <GalleryVerticalEnd size={20} />
+              )}
+            </Button>
+          )}
+        </div>
+
+        {/* Navigation link */}
+        {/* Affiche les liens de navigation sauf sur les appareils mobiles */}
+        {!isMobile && (
+          <ul className="flex gap-5 text-sm cursor-pointer">
+            {navItemsDesktop.map((item) => (
+              <NavItem
+                key={item.href}
+                href={item.href}
+                classname={"hover:text-orange-500"}
+              >
+                {item.label}
+              </NavItem>
+            ))}
+          </ul>
+        )}
+
+        {/* Mobile navigation */}
+        {/* Affiche les liens de navigation sur les appareils mobiles */}
+        {isMobile && isMobileMenuOpen && (
+          <div className={styles.menuMobile__container}>
+            <ul className={styles.menuMobile__content}>
+              {navItemsMobile.map((item) => (
+                <NavItem
+                  key={item.href}
+                  href={item.href}
+                  classname={styles.menuMobile__list}
+                >
+                  <item.icon size={17} />
+                  {item.label}
+                </NavItem>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {/* Navigation buttons */}
+        <div className="flex items-center gap-2">
+          {/* Ne pas afficher les boutons de navigations en version mobile */}
+          {!isMobile && (
+            <>
+              <Button className={styles.buttonPrimary}>Login</Button>
+              <Button className={styles.buttonSecondary}>Signup</Button>
+            </>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
 
 interface NavItemProps {
-    href: string,
-    children: React.ReactNode
+  href: string;
+  children: React.ReactNode;
+  classname: string | "";
 }
 
-const NavItem: React.FC<NavItemProps> = ({ href, children }) => {
-    return (
-        <li>
-            <Link to={href} className="hover:text-orange-500">{children}</Link>
-        </li>
-    )
-}
+const NavItem: React.FC<NavItemProps> = ({ href, children, classname }) => {
+  return (
+    <li>
+      <Link to={href} className={`${classname}`}>
+        {children}
+      </Link>
+    </li>
+  );
+};
 
-export default NavBar
+export default NavBar;
