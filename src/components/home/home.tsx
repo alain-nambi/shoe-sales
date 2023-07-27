85698cc7e3a59211de90e11a7159c7f877f5d01e
alain-nambi <alainnambi@gmail.com>
2023-07-27 16:28:24 +0300

    update(home ui) * Add SkeletonProductItem component * Add lazy component loading for Product

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,13 +1,23 @@
 import NavBar from "../navbar/navbar/navbar"
 import Banner from "../navbar/banner/banner"
-import Product from "../product/product"
+
+import SkeletonProductItem from "../product/skeleton-product-item"
+
+import {lazy, Suspense} from "react"
+
+const Product = lazy(() => import("../product/product"))
 
 const Home: React.FC = () => {
     return (
         <>
             <NavBar />
             <Banner />
-            <Product />
+            <div>
+                <p className="mt-4 font-bold max-sm:text-base md:text-lg max-sm:text-center"> Nouvelles arrivages</p>
+            </div>
+            <Suspense fallback={<SkeletonProductItem />}>
+                <Product />
+            </Suspense>
         </>
     )
 }
