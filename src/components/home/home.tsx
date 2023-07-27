aded5a0cb50d4f5317403bcb4ab61e4d61cdb40f
alain-nambi <alainnambi@gmail.com>
2023-07-27 14:29:36 +0300

    update: add product component to home component

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,11 +1,13 @@
 import NavBar from "../navbar/navbar/navbar"
 import Banner from "../navbar/banner/banner"
+import Product from "../product/product"
 
 const Home: React.FC = () => {
     return (
         <>
             <NavBar />
             <Banner />
+            <Product />
         </>
     )
 }
