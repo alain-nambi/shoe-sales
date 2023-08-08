6ddda27541c18f453ae6d83f739e964121d83ad3
alain-nambi <alainnambi@gmail.com>
2023-08-08 15:34:34 +0300

    update (banner ui) * Add SwiperSlide

--- src/components/navbar/banner/banner.tsx
+++ src/components/navbar/banner/banner.tsx
@@ -3,23 +3,80 @@ import { Button } from "../../ui/button";
 
 import styles from "./banner.module.css"
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import Swiper core and required modules
+import { Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const Banner: React.FC = () => {
+    
     return (
-        <section className={styles.banner}>
-            <div className={styles.banner__content}>
-                <h1 className={styles.banner__title}>
-                    Découvrez notre collection exclusive
-                </h1>
-                <p className={styles.banner__subtitle}>
-                    Trouvez les produits parfaits pour vous
-                </p>
-                <Link to="/category">
-                    <Button className={`button__primaryOutline ${styles.banner__button}`}>
-                        Découvrir maintenant                            
-                    </Button>
-                </Link>
-            </div>
-        </section>
+            <Swiper
+                modules={[Pagination, Scrollbar, A11y]}
+                spaceBetween={20}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+            >
+                <SwiperSlide className="mb-10">
+                    <section className={styles.banner}>
+                        <div className={styles.banner__content}>
+                            <h1 className={styles.banner__title}>
+                                Découvrez notre collection exclusive
+                            </h1>
+                            <p className={styles.banner__subtitle}>
+                                Trouvez les produits parfaits pour vous
+                            </p>
+                            <Link to="/category">
+                                <Button className={`button__primaryOutline ${styles.banner__button}`}>
+                                    Découvrir maintenant                            
+                                </Button>
+                            </Link>
+                        </div>
+                    </section>
+                </SwiperSlide>
+                <SwiperSlide className="mb-10">
+                    <section className={styles.banner}>
+                        <div className={styles.banner__content}>
+                            <h1 className={styles.banner__title}>
+                                Découvrez notre collection exclusive
+                            </h1>
+                            <p className={styles.banner__subtitle}>
+                                Trouvez les produits parfaits pour vous
+                            </p>
+                            <Link to="/category">
+                                <Button className={`button__primaryOutline ${styles.banner__button}`}>
+                                    Découvrir maintenant                            
+                                </Button>
+                            </Link>
+                        </div>
+                    </section>
+                </SwiperSlide>
+                <SwiperSlide className="mb-10">
+                    <section className={styles.banner}>
+                        <div className={styles.banner__content}>
+                            <h1 className={styles.banner__title}>
+                                Découvrez notre collection exclusive
+                            </h1>
+                            <p className={styles.banner__subtitle}>
+                                Trouvez les produits parfaits pour vous
+                            </p>
+                            <Link to="/category">
+                                <Button className={`button__primaryOutline ${styles.banner__button}`}>
+                                    Découvrir maintenant                            
+                                </Button>
+                            </Link>
+                        </div>
+                    </section>
+                </SwiperSlide>
+            </Swiper>
     )
 }
 
