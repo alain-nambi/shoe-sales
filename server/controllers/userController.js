997393046363ccac192c9557763d5f945e1d203b
alain-nambi <alainnambi@gmail.com>
2023-08-18 15:43:40 +0300

    server(controller): update userController * Make synchronous function to handle the error result * Add getUser to get user by userId

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,46 @@
 const userModel = require("../models/userModel")
 
 // Get all users 
-const getAllusers = async (req, res, next) => {
+const getAllusers = (_req, res, _next) => {
     try {   
-        await userModel
+        userModel
             .findAll()
             .then((users) => {
-                res.json({users: users})
+                res
+                    .status(200)
+                    .json({users: users})
             }).catch((error) => {
                 console.log(`Error on getting all users : ${error}`);
             });
-        
     } catch (error) {
         console.log(`Error on getting all users : ${error}`);
     }
 }
 
+const getUser = (req, res, _next) => {
+    try {
+        const userId = req.params.userId
+        if (userId) {
+            userModel
+                .findByPk(userId)
+                .then(user => {
+                    if (user) {
+                        res
+                            .status(200)
+                            .json({user: user})
+                    } else {
+                        res
+                            .status(404)
+                            .json({message: `User with userId ${userId} is not found`})
+                    }
+                })
+        }
+    } catch (error) {
+        console.log(`Error on get user by userId : ${error}`);
+    }
+}
+
 module.exports = {
-    getAllusers
+    getAllusers,
+    getUser,
 }
