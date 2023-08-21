f15d0241770b7918d7631f271a8d1afb6a1d153a
alain-nambi <alainnambi@gmail.com>
2023-08-21 11:35:45 +0300

    server(routes): update userRoutes * Add route for deleting users

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,16 @@
 const userController = require("../controllers/userController")
 const router = require("express").Router()
 
-// Get all users 
+// Get all users endpoint 
 router.get("/users", userController.getAllUsers)
 
-// Get one user
+// Get one user endpoint
 router.get("/users/:userId", userController.getUser)
 
-// Create user
+// Create user endpoint
 router.post("/users/create", userController.createUser)
+
+// Delete user endpoint
+router.delete("/users/delete/:userId", userController.deleteUser)
 
 module.exports = router
